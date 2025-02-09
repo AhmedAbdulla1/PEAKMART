2a74afe6e4af27840fd19050ae9475e075bb4d4c
KarimTamer74 <kemotamer448@gmail.com>
2025-02-09 21:31:37 +0200

    make text form field more customizable

--- lib/features/auth/presentation/shared_widgets/custom_text_form_field.dart
+++ lib/features/auth/presentation/shared_widgets/custom_text_form_field.dart
@@ -4,32 +4,37 @@ import 'package:peakmart/core/resources/color_manager.dart';
 import 'package:peakmart/core/resources/font_manager.dart';
 import 'package:peakmart/core/resources/string_manager.dart';
 import 'package:peakmart/core/resources/style_manager.dart';
+import 'package:peakmart/core/resources/values_manager.dart';
 
 class CustomTextFormField extends StatelessWidget {
   final String labelText;
   final String hintText;
-  final IconData iconData;
+  final IconData? iconData;
   final TextInputType inputType;
   final TextEditingController controller;
-
+  final bool? isShowDescription;
+  final bool? isUsedWithBidOwner;
   const CustomTextFormField({
     super.key,
     required this.labelText,
     required this.hintText,
-    required this.iconData,
+    this.iconData,
     required this.inputType,
     required this.controller,
+    this.isShowDescription,
+    this.isUsedWithBidOwner,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      maxLines: isShowDescription == true ? null : 1,
+      keyboardType: inputType,
       style: getRegularStyle(
         color: ColorManager.primary,
         fontSize: FontSize.s16,
       ),
-      keyboardType: inputType,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       validator: (value) {
         if (hintText == AppStrings.emailHint) {
@@ -62,16 +67,25 @@ class CustomTextFormField extends StatelessWidget {
         return null;
       },
       decoration: InputDecoration(
+        floatingLabelBehavior: isUsedWithBidOwner == true
+            ? FloatingLabelBehavior.never
+            : FloatingLabelBehavior.always,
         labelText: labelText,
         hintText: hintText,
+        contentPadding: isShowDescription == true
+            ? const EdgeInsets.only(top: AppPadding.p100, left: AppPadding.p20)
+            : const EdgeInsets.all(AppPadding.p20),
         prefixIconColor: ColorManager.lightGrey,
-        prefixIcon: Padding(
-          padding: const EdgeInsets.only(left: 5.0),
-          child: Icon(
-            iconData,
-            size: 28.sp,
-          ),
-        ),
+        prefixIcon: iconData != null
+            ? Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: Icon(
+                  iconData,
+                  size: 28.sp,
+                ),
+              )
+            : null,
+        alignLabelWithHint: true,
       ),
     );
   }

1e446e58138cb528fdb6e6c178e5d9cef13a0e65
KarimTamer74 <kemotamer448@gmail.com>
2025-02-17 18:32:32 +0200

    create future bids response

---
+++ lib/features/home/data/model/response/future_bids_response.dart
@@ -0,0 +1,59 @@
+import 'package:peakmart/core/models/base_model.dart';
+import 'package:peakmart/features/home/domain/entity/future_bids_entity.dart';
+
+class FutureBidsResponse extends BaseResponse<FutureBidsEntity> {
+  final List<FutureDataResponse> data;
+
+  FutureBidsResponse(
+      {required this.data,
+      required super.message,
+      required super.status,
+      required super.code});
+
+  factory FutureBidsResponse.fromJson(Map<String, dynamic> json) {
+    return FutureBidsResponse(
+      data: List<FutureDataResponse>.from(json["data"]
+          .map((futureData) => FutureDataResponse.fromJson(futureData))),
+      message: json["message"],
+      status: json["status"],
+      code: 200,
+    );
+  }
+
+  @override
+  FutureBidsEntity toEntity() {
+    return FutureBidsEntity(
+      data: data.map((futureData) => futureData.toEntity()).toList(),
+    );
+  }
+}
+
+class FutureDataResponse {
+  final String itemName, description, startDate, status;
+  final String? itemImage;
+  const FutureDataResponse({
+    this.itemImage,
+    required this.itemName,
+    required this.description,
+    required this.startDate,
+    required this.status,
+  });
+
+  factory FutureDataResponse.fromJson(Map<String, dynamic> json) {
+    return FutureDataResponse(
+        itemName: json["ITEM_NAME"],
+        description: json["DESCRIPTION"],
+        itemImage: json["PHOTO"],
+        startDate: json["START_DATE"],
+        status: json["STATUS"]);
+  }
+
+  FutureBidsData toEntity() {
+    return FutureBidsData(
+        itemName: itemName,
+        description: description,
+        itemImage: itemImage,
+        startDate: startDate,
+        status: status);
+  }
+}

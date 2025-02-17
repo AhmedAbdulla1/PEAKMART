5f6de19bfa34071bcc14dcd8d7007267ad69075e
KarimTamer74 <kemotamer448@gmail.com>
2025-02-17 17:20:27 +0200

    initate add product handle request

---
+++ lib/features/bid_owner/data/models/request/add_product_request.dart
@@ -0,0 +1,11 @@
+import 'package:peakmart/core/requests/base_request.dart';
+
+class AddProductRequest extends BaseRequest {
+  @override
+  void printRequest() {}
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+}

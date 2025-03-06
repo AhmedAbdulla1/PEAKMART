f0f42a2bb1cb5756388565f8a2b053dfe5bc1645
KarimTamer74 <kemotamer448@gmail.com>
2025-03-06 03:14:53 +0200

    refactor custom app bar

--- lib/features/auth/presentation/shared_widgets/custom_appbar.dart
+++ lib/features/auth/presentation/shared_widgets/custom_appbar.dart
@@ -1,5 +1,5 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:peakmart/core/resources/assets_manager.dart';
 import 'package:peakmart/core/resources/color_manager.dart';
@@ -8,8 +8,15 @@ import 'package:peakmart/core/resources/style_manager.dart';
 import 'package:peakmart/core/resources/values_manager.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key, required this.title});
+  const CustomAppBar(
+      {super.key,
+      required this.title,
+      this.actions,
+      this.centerTitle,
+      this.isNotShowArrowBack = false});
   final String title;
+  final List<Widget>? actions;
+  final bool? centerTitle, isNotShowArrowBack;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,26 +29,33 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         backgroundColor: ColorManager.white,
         elevation: 0,
         forceMaterialTransparency: true,
-        leading: Center(
-          child: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: SvgPicture.asset(IconsAssets.arrowBack),
-            style: IconButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
-                side: BorderSide(color: ColorManager.grey2),
+        leading: isNotShowArrowBack == false
+            ? Center(
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: SvgPicture.asset(IconsAssets.arrowBack),
+                  style: IconButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                      side: BorderSide(color: ColorManager.grey2),
+                    ),
+                  ),
+                ),
+              )
+            : const SizedBox(
+                width: 1,
+                height: 1,
               ),
-            ),
-          ),
-        ),
+        leadingWidth: isNotShowArrowBack == true ? 0 : 50.w,
         title: Text(
           title,
           style: getSemiBoldStyle(
-              fontSize: FontSize.s18, color: ColorManager.black),
+              fontSize: FontSize.s20, color: ColorManager.black),
         ),
-        centerTitle: true,
+        centerTitle: centerTitle ?? true,
+        actions: actions,
       ),
     );
   }
@@ -50,5 +64,29 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize =>
       const Size.fromHeight(56.0); // Default AppBar height
 }
-
+// AppBar(
+//         centerTitle: false,
+//         title: Text("My Products",
+//             style: getBoldStyle(
+//                 fontSize: FontSize.s24, color: ColorManager.black)),
+//         actions: [
+//           Padding(
+//             padding: const EdgeInsets.only(right: 8),
+//             child: IconButton(
+//               onPressed: () {
+//                 Navigator.pushNamed(context, BidOwnerView.routeName);
+//               },
+//               tooltip: "Add Product",
+//               icon: const Icon(
+//                 Icons.add,
+//                 color: ColorManager.white,
+//               ),
+//               style:
+//                   IconButton.styleFrom(backgroundColor: ColorManager.primary),
+//             ),
+//           ),
+//         ],
+//         backgroundColor: Colors.white,
+//         elevation: 0,
+//       ),
 

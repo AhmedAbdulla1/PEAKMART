b3c8a44ffb9d807850bac81bf21637c6b12db12c
KarimTamer74 <kemotamer448@gmail.com>
2025-04-15 17:09:48 +0200

    fix theme mode issue

--- lib/core/resources/theme/app_theming_cubit/app_theme_cubit.dart
+++ lib/core/resources/theme/app_theming_cubit/app_theme_cubit.dart
@@ -1,44 +1,35 @@
 import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class AppThemeCubit extends HydratedCubit<ThemeMode> {
-  AppThemeCubit() : super(ThemeMode.system) {
-    final savedTheme = fromJson(HydratedBloc.storage.read('themeMode') ?? {});
-    if (savedTheme == null) {
-      final isSystemDarkMode =
-          WidgetsBinding.instance.platformDispatcher.platformBrightness ==
-              Brightness.dark;
-      changeTheme(isSystemDarkMode ? ThemeMode.dark : ThemeMode.light,
-          isDarkMode: isSystemDarkMode);
-    } else {
-      emit(savedTheme);
-    }
-  }
-
-  bool isDarkMode = false;
-
-  void changeTheme(ThemeMode themeMode, {required bool isDarkMode}) {
-    log('Theme changed to: $themeMode');
-    this.isDarkMode = isDarkMode;
+  AppThemeCubit() : super(ThemeMode.system);
+  void changeTheme(ThemeMode themeMode) {
+    log(themeMode.toString());
     emit(themeMode);
   }
 
   final String themeModeKey = 'themeMode';
-
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
-    final theme = json[themeModeKey];
-    isDarkMode = theme == 'dark';
-    return theme == 'dark'
-        ? ThemeMode.dark
-        : theme == 'light'
-            ? ThemeMode.light
-            : ThemeMode.system;
+    if (json[themeModeKey] == 'dark') {
+      return ThemeMode.dark;
+    } else if (json[themeModeKey] == 'light') {
+      return ThemeMode.light;
+    } else {
+      return ThemeMode.system;
+    }
   }
 
   @override
   Map<String, dynamic>? toJson(ThemeMode state) {
-    return {themeModeKey: state == ThemeMode.dark ? 'dark' : 'light'};
+    if (state == ThemeMode.dark) {
+      return {themeModeKey: 'dark'};
+    } else if (state == ThemeMode.light) {
+      return {themeModeKey: 'light'};
+    } else {
+      return {themeModeKey: 'system'};
+    }
   }
 }

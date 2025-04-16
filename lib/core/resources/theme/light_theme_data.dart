3e719f9b9164652c26e89020dc3cd00aa03c7c0d
KarimTamer74 <kemotamer448@gmail.com>
2025-04-16 20:03:49 +0200

    handle shimmer color in light mode

--- lib/core/resources/theme/light_theme_data.dart
+++ lib/core/resources/theme/light_theme_data.dart
@@ -12,16 +12,15 @@ ThemeData getLightTheme() {
     extensions: [
       SkeletonizerConfigData(
           effect: ShimmerEffect(
-            baseColor: ColorManager.white, // Dark grey for dark mode
-            highlightColor: ColorManager.grey2, // Lighter grey highlight
+            baseColor: ColorManager.grey2, // Dark grey for dark mode
+            highlightColor: ColorManager.white, // Lighter grey highlight
             duration: const Duration(milliseconds: 800),
           ),
           enableSwitchAnimation: true,
-          switchAnimationConfig: SwitchAnimationConfig(
+          switchAnimationConfig: const SwitchAnimationConfig(
             duration: const Duration(milliseconds: 1000),
             switchOutCurve: Curves.easeOut,
-          )
-      ),
+          )),
     ],
     scaffoldBackgroundColor: ColorManager.white,
     primaryColor: ColorManager.primary,

af1c267ccd816ceb5fb16dba39bc0f606f3215d9
KarimTamer74 <kemotamer448@gmail.com>
2025-04-25 15:01:56 +0300

    run release

--- lib/features/auth/presentation/views/otp_verification/otp_verification_body.dart
+++ lib/features/auth/presentation/views/otp_verification/otp_verification_body.dart
@@ -86,11 +86,32 @@ class _OtpVerificationBodyState extends State<OtpVerificationBody> {
             Navigator.pushReplacementNamed(context, HoldScreen.routeName);
           } else if (state is OtpVerificationFailureState) {
             log('Failure state');
-            // Navigator.pop(context);
+            // Navigator.pop(context); 
             ErrorViewer.showError(
                 errorViewerOptions: ErrVToastOptions(
                   textColor: ColorManager.white,
-                  backGroundColor: ColorManager.lightGrey,
+                  backGroundColor: ColorManager.lightGrey, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
                 ),
                 context: context,
                 error: state.errors,

995471d0a48fe93a00b2e9384341253014187564
KarimTamer74 <kemotamer448@gmail.com>
2025-05-17 17:06:02 +0300

    fix isssue of loading indicator in light mode

--- lib/core/widgets/waiting_widget.dart
+++ lib/core/widgets/waiting_widget.dart
@@ -8,8 +8,8 @@ class WaitingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator.adaptive(
-        backgroundColor: ColorManager.primary,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(ColorManager.primary),
       ),
     );
   }

1d286bcb5c9c6d39fd4618a6119c27f4ab5ba6b6
KarimTamer74 <kemotamer448@gmail.com>
2025-05-19 20:46:44 +0300

    add refresh indicator to uploaded and enrolled products

--- lib/features/profile/presentation/views/user_products/widgets/product_uploaded_item_widget.dart
+++ lib/features/profile/presentation/views/user_products/widgets/product_uploaded_item_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:peakmart/core/entities/prodcut_entity.dart';
 import 'package:peakmart/core/resources/color_manager.dart';
 import 'package:peakmart/core/resources/extentions.dart';
 import 'package:peakmart/core/resources/font_manager.dart';
 import 'package:peakmart/core/resources/style_manager.dart';
+import 'package:peakmart/core/resources/theme/extentaions/app_theme_ext.dart';
 import 'package:peakmart/features/products/presentation/views/product_details/product_details_view.dart';
 import 'package:peakmart/features/products/presentation/views/product_details/widgets/product_details_view_body.dart';
+import 'package:peakmart/features/profile/presentation/state_m/user_products/user_products_cubit.dart';
+import 'package:peakmart/features/profile/presentation/views/user_products/widgets/cancel_and_end_product.dart';
 import 'package:peakmart/features/profile/presentation/views/user_products/widgets/product_images_slider.dart';
 
 class ProductsUploadedItemWidget extends StatelessWidget {
@@ -21,28 +25,53 @@ class ProductsUploadedItemWidget extends StatelessWidget {
   final ProductEntity product;
   final bool? isUsingWithRandomProducts;
 
+  String _getEndDate() {
+    final startDate = DateTime.parse(product.startDate!);
+    final endDate = startDate.add(Duration(days: product.periodOfBid!));
+    return "${endDate.year.toString().padLeft(4, '0')}-"
+        "${endDate.month.toString().padLeft(2, '0')}-"
+        "${endDate.day.toString().padLeft(2, '0')}";
+  }
+
+  Widget _buildStatusWidget(BuildContext context) {
+    final status = product.status ?? "";
+    switch (status) {
+      case "ended":
+        return _statusText("Ended", ColorManager.primary);
+      case "canceled":
+        return _statusText("Canceled", ColorManager.red);
+      default:
+        return BlocProvider(
+          create: (context) => UserProductsCubit(),
+          child:  CancelAndEndButtonsWidget(productId:product.id),
+        );
+    }
+  }
+
+  Widget _statusText(String text, Color color) {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        text,
+        style: getMediumStyle(
+          color: color,
+          fontSize: 22,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    String getEndDate() {
-      DateTime startDate = DateTime.parse(product.startDate!);
-
-      DateTime endDate = startDate.add(Duration(days: product.periodOfBid!));
-
-      String endDateString = "${endDate.year.toString().padLeft(4, '0')}-"
-          "${endDate.month.toString().padLeft(2, '0')}-"
-          "${endDate.day.toString().padLeft(2, '0')}";
-      return endDateString;
-    }
-
     return InkWell(
-      onTap: () {
-        Navigator.pushNamed(context, ProductDetails.routeName,
-            arguments: product.id);
-      },
+      onTap: () => Navigator.pushNamed(
+        context,
+        ProductDetails.routeName,
+        arguments: product.id,
+      ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-          // margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 16.h),
           clipBehavior: Clip.antiAliasWithSaveLayer,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20.r),
@@ -51,6 +80,7 @@ class ProductsUploadedItemWidget extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Product image or placeholder
               ClipRRect(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(18.r),
@@ -65,74 +95,50 @@ class ProductsUploadedItemWidget extends StatelessWidget {
                         fit: BoxFit.cover,
                       ),
               ),
+
+              // Product details
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 10.h),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    // Product name
                     Text(
                       product.name,
-                      style: getBoldStyle(
-                        fontSize: FontSize.s16,
-                      ),
+                      style: getBoldStyle(fontSize: FontSize.s16),
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
                     ),
+
+                    // Product description
                     Text(
                       product.description,
                       style: getMediumStyle(
-                          fontSize: FontSize.s12, color: ColorManager.grey),
+                        fontSize: FontSize.s12,
+                        color: context.isDarkMode
+                            ? ColorManager.grey
+                            : ColorManager.grey1,
+                      ),
                       maxLines: 2,
                       overflow: TextOverflow.ellipsis,
                     ),
                     6.vGap,
+
+                    // End date and price
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         CustomRichText(
-                            title: "End Date: ", description: getEndDate()),
+                            title: "End Date: ", description: _getEndDate()),
                         CustomRichText(
                             title: "Price Now: \$",
                             description: product.price.toString()),
                       ],
                     ),
                     10.vGap,
-                    Row(
-                      children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 40.h,
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
-                                'End',
-                                style: getBoldStyle(
-                                  fontSize: FontSize.s16,
-                                  color: ColorManager.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        8.hGap,
-                        Expanded(
-                          child: SizedBox(
-                            height: 40.h,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Cancel',
-                                style: getBoldStyle(
-                                  fontSize: FontSize.s16,
-                                  color: ColorManager.primary,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    10.vGap,
+
+                    // Status or buttons
+                    _buildStatusWidget(context),
                   ],
                 ),
               ),

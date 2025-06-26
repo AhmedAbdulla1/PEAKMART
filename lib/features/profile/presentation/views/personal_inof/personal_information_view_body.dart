98d06232f8e5c0b9f8a2021575793fcd90338f21
KarimTamer74 <kemotamer448@gmail.com>
2025-06-26 23:20:37 +0300

    active change password button

--- lib/features/profile/presentation/views/personal_inof/personal_information_view_body.dart
+++ lib/features/profile/presentation/views/personal_inof/personal_information_view_body.dart
@@ -7,6 +7,7 @@ import 'package:peakmart/app/di.dart';
 import 'package:peakmart/core/resources/extentions.dart';
 import 'package:peakmart/core/resources/values_manager.dart';
 import 'package:peakmart/core/shared_widgets/buttons.dart';
+import 'package:peakmart/features/auth/presentation/views/reset_password/forget_password_view.dart';
 import 'package:peakmart/features/profile/presentation/state_m/profile/cubit.dart';
 import 'package:peakmart/features/profile/presentation/views/personal_inof/passwrod_dialog.dart';
 import 'package:peakmart/features/profile/presentation/views/personal_inof/personal_info_form.dart';
@@ -115,7 +116,12 @@ class _PersonalInformationViewBodyState
               children: [
                 CustomElevatedButtonWithoutStream(
                   height: AppSize.s45,
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      ForgotPasswordView.routeName,
+                    );
+                  },
                   text: "Change Password",
                 ),
                 15.verticalSpace,
